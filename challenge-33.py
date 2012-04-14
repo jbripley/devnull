af39c85ab57806ed24c20628ddda3d29b5012bd6
Joakim Bodin <joakim.bodin@gmail.com>
2012-04-14 14:57:23 +0200

    Add solution for challegne 33 with input data

---
+++ challenge-33.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*- 
+
+import urllib2
+
+startMatchRowStart = 100101
+startMatchRow = 100101
+with open('2_8.data.txt', 'r') as f:
+    for i, line in enumerate(f):
+        if i == startMatchRow:
+            startMatchRow += 100101
+            print "row: %s" % line
