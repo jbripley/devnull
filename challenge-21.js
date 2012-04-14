7fca72ac99e5b1cf9c2e17bfca074da2bea2a585
Joakim Bodin <joakim.bodin@gmail.com>
2012-04-14 16:22:47 +0200

    Add data for challenge 21

---
+++ challenge-21.js
@@ -0,0 +1,16 @@
+{
+	"table1":
+		[
+			{"id": 1, "label": "container1", "parent": null},
+			{"id": 1, "label": "container2", "parent": 1},
+			{"id": 1, "label": "container3", "parent": 1},
+			{"id": 1, "label": "container4", "parent": 2}
+		],
+	"table2":
+		[
+			{"id": 1, "label": "container1", "parent": 1},
+			{"id": 1, "label": "container1", "parent": 4},
+			{"id": 1, "label": "container1", "parent": 4},
+			{"id": 1, "label": "container1", "parent": 3}
+		]
+}
